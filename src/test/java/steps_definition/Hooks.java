9ef4ef76b25cb1e9bc9e004a63055ac4bb6a2f26
David Cardenas Guzman <davidcarguz@gmail.com>
2020-04-01 10:44:29 -0500

    creación caso login en twitter

---
+++ src/test/java/steps_definition/Hooks.java
@@ -0,0 +1,33 @@
+package steps_definition;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import Utilidades.BaseUtil;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks extends BaseUtil{
+
+	private BaseUtil base;
+	private ChromeOptions options;
+	
+	public Hooks(BaseUtil base) {
+		this.base = base;
+	}
+
+	@Before
+	public void inicializarTest() {
+		WebDriverManager.chromedriver().setup();
+		this.options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		this.base.driver = new ChromeDriver(options);	
+	}
+	
+	@After
+	public void finalizarTest() {
+		this.base.driver.quit();
+	}
+	
+}
--- src/test/java/steps_definition/LoginSteps.java
+++ src/test/java/steps_definition/LoginSteps.java
@@ -1,15 +1,32 @@
 package steps_definition;
 
-import io.cucumber.java8.*;
-import page_object.Browser;
 
-public class LoginSteps implements En {
-	Browser browser;
+import static org.junit.Assert.assertTrue;
+
+import Utilidades.BaseUtil;
+import io.cucumber.java8.*;
+import page_object.TwitterMainPage;
+
+public class LoginSteps extends BaseUtil implements En {
+
+	private BaseUtil base;
 	
-	public LoginSteps() {
-		Given("I want to open Twitter in a browser", ()->{
-			browser = new Browser();
-			browser.open();
+	TwitterMainPage twitterMainPage;
+	
+	public LoginSteps(BaseUtil base) {
+		this.base = base;
+		
+		Given("I want to navigate to twitter main page", () -> {
+			this.twitterMainPage = new TwitterMainPage(this.base);
+			twitterMainPage.navigateToTwitter();});
+		
+		When("I want to log into Twitter with username {string} and password {string}", (String username, String password) -> {
+			twitterMainPage.logInWithUsernameAndPassword(username,password);
+		});
+		
+		Then("I validate the successful login into Twitter",() ->{
+			assertTrue(twitterMainPage.homeButtonIsPresent());
 		});
 	}
+		
 }
