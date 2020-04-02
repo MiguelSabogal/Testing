b0a4804d5c2a539881c95b3b3b446b144317d2f4
David Cardenas Guzman <davidcarguz@gmail.com>
2020-04-02 09:41:25 -0500

    cambio en selector de elemento BTN_LOGIN2

--- src/test/java/page_object/TwitterMainPage.java
+++ src/test/java/page_object/TwitterMainPage.java
@@ -15,7 +15,7 @@ public class TwitterMainPage{
 	private final String BTN_LOGIN = "//span[contains(text(),'Iniciar sesión')]"; 
 	private final String EDT_USER = "session[username_or_email]";
 	private final String EDT_PASSWORD = "session[password]";
-	private final String BTN_LOGIN2 = "//div[@class='css-18t94o4 css-1dbjc4n r-urgr8i r-42olwf r-sdzlij r-1phboty r-rs99b7 r-1w2pmg r-vlx1xi r-zg41ew r-1jayybb r-17bavie r-15bsvpr r-o7ynqc r-6416eg r-lrvibr']//div[@class='css-901oao r-1awozwy r-jwli3a r-6koalj r-18u37iz r-16y2uox r-1qd0xha r-a023e6 r-vw2c0b r-1777fci r-eljoum r-dnmrzs r-bcqeeo r-q4m81j r-qvutc0']";
+	private final String BTN_LOGIN2 = "div[class$='r-bcqeeo r-q4m81j r-qvutc0']";
 	private final String BTN_HOME = "//span[contains(text(),'Home')]";
 	//----------------------------------------------//
 
@@ -49,7 +49,7 @@ public class TwitterMainPage{
 	}
 	
 	public void loginWithButton() {
-		WebElement btnLogin2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(BTN_LOGIN2)));
+		WebElement btnLogin2 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(BTN_LOGIN2)));
 		btnLogin2.click();
 		
 	}
